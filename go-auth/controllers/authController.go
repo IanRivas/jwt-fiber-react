f943f2c4be605d9fdc188fecf5cecf3a5530219a
Ian Rivas <rivas.ian.javier@gmail.com>
2022-03-08 23:04:02 -0300

    register and login before jwt

---
+++ go-auth/controllers/authController.go
@@ -0,0 +1,54 @@
+package controllers
+
+import (
+	"github.com/IanRivas/jwt-fiber-react/database"
+	"github.com/IanRivas/jwt-fiber-react/models"
+	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func Register(c *fiber.Ctx) error {
+	var data map[string]string
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+	password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 14)
+	user := models.User{
+		Name:     data["name"],
+		Email:    data["email"],
+		Password: password,
+	}
+
+	database.DB.Create(&user)
+	return c.JSON(user)
+}
+
+func Login(c *fiber.Ctx) error {
+	var data map[string]string
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	var user models.User
+
+	// en user le metemos lo que encuentra en la base de datos,
+	// sino encuentra nada user va a tener los valores por defecto por eso == 0
+	database.DB.Where("email = ?", data["email"]).First(&user)
+	if user.Id == 0 {
+		c.Status(fiber.StatusNotFound)
+		return c.JSON(fiber.Map{
+			"message": "user not found",
+		})
+	}
+	// en data con el bodyparser, es decir sacamos la info del json y user ,es lo que encontro la DB
+	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err != nil {
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"message": "incorrect password",
+		})
+	}
+
+	// la contraseña es correcta si llegamos aca
+	return c.JSON(user)
+
+}
