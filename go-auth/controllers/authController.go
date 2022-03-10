ed88786a096f06dc6789d22f83eb7f7aeec2dd66
Ian Rivas <rivas.ian.javier@gmail.com>
2022-03-10 16:21:38 -0300

    login finished

--- go-auth/controllers/authController.go
+++ go-auth/controllers/authController.go
@@ -1,12 +1,19 @@
 package controllers
 
 import (
+	"strconv"
+	"time"
+
 	"github.com/IanRivas/jwt-fiber-react/database"
 	"github.com/IanRivas/jwt-fiber-react/models"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
 	"golang.org/x/crypto/bcrypt"
 )
 
+const SecretKey = "secret"
+
+// Register es el handler de un POST , que toma el usuario del body y lo registra
 func Register(c *fiber.Ctx) error {
 	var data map[string]string
 	if err := c.BodyParser(&data); err != nil {
@@ -48,7 +55,71 @@ func Login(c *fiber.Ctx) error {
 		})
 	}
 
-	// la contraseña es correcta si llegamos aca
-	return c.JSON(user)
+	// userId lo pasamod a numero y despues a string
+	// token expiresat despues de un dia
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		Issuer:    strconv.Itoa(int(user.Id)),
+		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
+	})
 
+	token, err := claims.SignedString([]byte(SecretKey))
+	if err != nil {
+		c.Status(fiber.StatusInternalServerError)
+		return c.JSON(fiber.Map{
+			"message": "Could not login",
+		})
+	}
+	//http only para que el frontend no tenga manera de acceder a la cookie
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    token,
+		Expires:  time.Now().Add(time.Hour * 24),
+		HTTPOnly: true,
+	}
+
+	c.Cookie(&cookie)
+	// la contraseña es correcta si llegamos aca
+	return c.JSON(fiber.Map{
+		"message": "success",
+	})
+
+}
+
+//leemos la cookie , en cookie esta el string que es el token y se lo pasamos a parsewithclaims
+// segundo parametro el claim que usamos, y al final una funcion en la que le pasamos el secret key
+func User(c *fiber.Ctx) error {
+	cookie := c.Cookies("jwt")
+
+	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(SecretKey), nil
+	})
+	if err != nil {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "unauthenticated",
+		})
+	}
+	// hacemos un type assertion para que sea de ese tipo , asi podemos leer el issuer del claims
+	// que es el id del usuario, y lo buscamos en la base de datos
+	claims := token.Claims.(*jwt.StandardClaims)
+	var user models.User
+	database.DB.Where("id = ?", claims.Issuer).First(&user)
+
+	return c.JSON(user)
+}
+
+// si no estamos logiados no podemos hacer el handler User - sale unautenticate
+func Logout(c *fiber.Ctx) error {
+	// la forma de remover cookies es decirle que expiro hace una hora
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    "",
+		Expires:  time.Now().Add(-time.Hour),
+		HTTPOnly: true,
+	}
+	c.Cookie(&cookie)
+
+	return c.JSON(fiber.Map{
+		"message": "success",
+	})
 }
--- go-auth/main.go
+++ go-auth/main.go
@@ -4,12 +4,18 @@ import (
 	"github.com/IanRivas/jwt-fiber-react/database"
 	"github.com/IanRivas/jwt-fiber-react/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	database.Connect()
 
 	app := fiber.New()
+
+	// Allow Credentials - necesitamos esto para que sirvan las cookies
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+	}))
 
 	routes.Setup(app)
 
